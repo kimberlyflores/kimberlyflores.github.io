17fc99b39e8c265aa275fb72491ea1fea6b70fa8
Kimberly Flores Gaynor <kfloresgaynor@users.noreply.github.com>
2019-10-16 19:51:55 -0400

    php
    contact

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
 <?php
-$mailTo = 'yourmail@yourmal.com';
+$mailTo = 'kfloresgaynor@gmail.com';
 $name = htmlspecialchars($_POST['idi_name']);
 $mailFrom = htmlspecialchars($_POST['idi_mail']);
-$subject = 'Website Feedback';
+$subject = 'Contact from Kimberly Flores Gaynor. website';
 $message_text = htmlspecialchars($_POST['idi_text']);
 $message =  'From: '.$name.'; Email: '.$mailFrom.' ; Message: '.$message_text;
 $headers .= 'From:' . $mailFrom . '\r\n';
